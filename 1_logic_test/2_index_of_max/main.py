7ddbbce7c2f5c96a8d60c269d58a1266c6be52cd
Pawat Limpanuthai <pawat.limpanuthai@gmail.com>
2026-06-10 22:00:47 +0700

    Submit find index of maximum value in list

--- 1_logic_test/2_index_of_max/main.py
+++ 1_logic_test/2_index_of_max/main.py
@@ -20,4 +20,35 @@ output = list can not blank
 class Solution:
 
     def find_max_index(self, numbers: list) -> int | str:
-        pass
+        if len(numbers) == 0:
+            return "list can not blank"
+        
+        max_value = max(numbers)
+        max_index = numbers.index(max_value)
+
+        return max_index
+
+    # ในกรณีที่อยากได้ index ของ max value ที่อยู่ใน list มากกว่า 1 ตัว
+    def find_max_indexs(self, numbers: list) -> list | str:
+        if len(numbers) == 0:
+            return "list can not blank"
+        
+        max_value = max(numbers)
+        result_indices = []
+
+        for i in range(len(numbers)):
+            if numbers[i] == max_value:
+                result_indices.append(i)
+
+        return result_indices
+    
+# python 1_logic_test\2_index_of_max\main.py
+sol = Solution()
+
+input = [1,2,1,3,5,6,4]
+result1 = sol.find_max_index(input)
+print(f"Input: {input} -> Output: {result1}")
+
+input = [1,2,1,3,5,6,4,6]
+result2 = sol.find_max_indexs(input)
+print(f"Input: {input} -> Output: {result2}")
